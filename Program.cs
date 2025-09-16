e407605a96ae315f3c0fd4eac81f77fce37db2fc
genesisvejar <genesisvejar09@gmail.com>
2025-09-15 23:43:20 -0400

    lab 3a

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿ using Cards2;
+using System;
+
+Card c1 = new Card(Rank.Ace, Suit.Diamonds);
+
+
+Console.WriteLine(c1.Rank + " " + c1.Suit);
+    
+    Console.WriteLine();
+
+    Deck d1 = new Deck();
+
+    d1.Shuffle();
+
+    Card topCard = d1.TakeTopCard();
+
+    Console.WriteLine("The top card is " + topCard.Rank + " of " + topCard.Suit);
+
+    /*topCard.Rank = Rank.Kings; it's read only so the rank can't be assigned
+    because it's declared like this:
+    public Rank Rank { get; }
+    and not like this:
+    public Rank Rank { get; set; } */
+
+    Console.WriteLine();
+
+    Console.Write("The shuffled deck is: \n");
+
+    d1.Print();
+
+
+    
